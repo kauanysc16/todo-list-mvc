e45c6b3b7602574e7a7e578a4b7a3b2865f6606e
kauanysc16 <kauanycamilo91@gmail.com>
2024-09-03 16:54:21 -0300

    03/9

---
+++ utils/mongodb.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+
+const connectMongo = async () => {
+    mongoose.connect(process.env.DATABASE_URL)//estabelecendo conexão
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch(err => console.error('Erro ao conectar ao MongoDB', err));
+}
+
+
+export default connectMongo;
+
+
+
+
